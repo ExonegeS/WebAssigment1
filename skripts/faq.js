6fa2c182178f15fcb74d162763da6fe7bcf09407
ExonegeS <2004gusak@gmail.com>
2024-10-11 00:16:09 +0500

    FAQ update, smooth

--- skripts/faq.js
+++ skripts/faq.js
@@ -14,21 +14,20 @@ function iterateFAQblock(faqBlock, level = 0) {
         }
         if (level > 0) {
             if (child.classList.contains("faq-answer")) {
-                child.hidden = true;
+                child.classList.add('collapsed');;
             }
         }
         if (child.classList.contains("faq-question")) {
             let answers = Array.from(faqBlock.children).filter((e) => {return e.classList.contains("faq-answer")})
-            child.onclick = function() { toggleAnswer(answers, child) };
+            child.onclick = function() { toggleAnswer(answers) };
         }
     }
 }
 
-function toggleAnswer(answers, block) {
+function toggleAnswer(answers) {
     for (var i = 0; i < answers.length; i++) {
         var child = answers[i];
-        //child.style.background = 'red';
-        child.hidden = !child.hidden;
+        child.classList.toggle('collapsed');
     }
 }
 
